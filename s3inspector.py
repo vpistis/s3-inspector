40177c4ad35ae86c7ad39a46ce9e194d8375a1ca
vpistis <vale.pistis@gmail.com>
2017-10-11 23:41:21 +0200

    added support for macos, added option to select aws-cli credentials profile name, added some doc string, minor changes

--- s3inspector.py
+++ s3inspector.py
@@ -1,83 +1,115 @@
+# -*- coding: utf-8 -*-
 import os
 import re
 import sys
-
 from collections import defaultdict
+
+import boto3
+import botocore
+import requests
+import termcolor
 
 
 def check_acl(acl):
+    """
+    Check if the Access Control List is public
+    :param acl:
+    :return:
+    """
     dangerous_grants = defaultdict(list)
-    for grant in acl.grants:
-        grantee = grant['Grantee']
+    for _grant in acl.grants:
+        grantee = _grant['Grantee']
         if grantee['Type'] == 'Group' and grantee['URI'] in groups_to_check:
-            dangerous_grants[grantee['URI']].append(grant['Permission'])
+            dangerous_grants[grantee['URI']].append(_grant['Permission'])
     public_indicator = True if dangerous_grants else False
     return public_indicator, dangerous_grants
 
 
 def get_location(bucket_name):
+    """
+    Return the bucket location
+
+    :param bucket_name:
+    :return:
+    """
     loc = s3_client.get_bucket_location(
-        Bucket=bucket_name)['LocationConstraint']
+            Bucket=bucket_name)["LocationConstraint"]
     if loc is None:
-        loc = 'None(probably North Virginia)'
+        loc = "None(probably North Virginia)"
     return loc
 
 
 def install_and_import(pkg):
+    """
+    Install latest versions of required packages
+
+    :param pkg:
+    :return:
+    """
     import importlib
     try:
         importlib.import_module(pkg)
     except ImportError:
         import pip
-        pip.main(['install', pkg])
+        pip.main(["install", pkg])
     finally:
         globals()[pkg] = importlib.import_module(pkg)
 
 
 def scan_bucket_urls(bucket_name):
-    domain = 's3.amazonaws.com'
+    """
+    Scan standard bucket urls.
+    Return only publicly accessible urls.
+
+    :param bucket_name:
+    :return:
+    """
+
+    domain = "s3.amazonaws.com"
     access_urls = []
     urls_to_scan = [
-        'https://{}.{}'.format(bucket_name, domain),
-        'http://{}.{}'.format(bucket_name, domain),
-        'https://{}/{}'.format(domain, bucket_name),
-        'http://{}/{}'.format(domain, bucket_name)
+        "https://{}.{}".format(bucket_name, domain),
+        "http://{}.{}".format(bucket_name, domain),
+        "https://{}/{}".format(domain, bucket_name),
+        "http://{}/{}".format(domain, bucket_name)
     ]
     for url in urls_to_scan:
         content = requests.get(url).text
-        if not re.search('Access Denied', content):
+        if not re.search("Access Denied", content):
             access_urls.append(url)
     return access_urls
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if sys.version[0] == "3":
         raw_input = input
-    packages = ['boto3', 'botocore', 'termcolor', 'requests']
+    packages = ["boto3", "botocore", "termcolor", "requests"]
     for package in packages:
         install_and_import(package)
-    SEP = '-' * 40
+    SEP = "-" * 40
     explained = {
-        'READ': 'readable',
-        'WRITE': 'writable',
-        'READ_ACP': 'permissions readable',
-        'WRITE_ACP': 'permissions writeable',
-        'FULL_CONTROL': 'Full Control'
+        "READ": "readable",
+        "WRITE": "writable",
+        "READ_ACP": "permissions readable",
+        "WRITE_ACP": "permissions writeable",
+        "FULL_CONTROL": "Full Control"
     }
     groups_to_check = {
-        'http://acs.amazonaws.com/groups/global/AllUsers': 'Everyone',
-        'http://acs.amazonaws.com/groups/global/AuthenticatedUsers': 'Authenticated AWS users'
+        "http://acs.amazonaws.com/groups/global/AllUsers": "Everyone",
+        "http://acs.amazonaws.com/groups/global/AuthenticatedUsers": "Authenticated AWS users"
     }
-    if (os.path.exists('/home/{}/.aws/credentials'.format(os.getlogin())) or
-            os.path.exists('/home/{}/.aws/config'.format(os.getlogin()))):
-        s3 = boto3.resource('s3')
-        s3_client = boto3.client('s3')
+    if os.path.exists("{}/.aws/credentials".format(os.environ["HOME"])) or os.path.exists(
+            "{}/.aws/config".format(os.environ["HOME"])):
+        profile_name = raw_input("Enter your AWS profile name [default]: ") or "default"
+        session = boto3.Session(profile_name=profile_name)
+        s3 = session.resource("s3")
+        s3_client = session.client("s3")
     else:
-        access_key = raw_input('Enter your AWS access key ID: ')
-        secret_key = raw_input('Enter your AWS secret key: ')
-        s3 = boto3.resource('s3', aws_access_key_id=access_key,
+        access_key = raw_input("Enter your AWS access key ID: ")
+        secret_key = raw_input("Enter your AWS secret key: ")
+        s3 = boto3.resource("s3", aws_access_key_id=access_key,
                             aws_secret_access_key=secret_key)
-        s3_client = boto3.client('s3', aws_access_key_id=access_key,
+        s3_client = boto3.client("s3", aws_access_key_id=access_key,
                                  aws_secret_access_key=secret_key)
     bucket_list = []
     buckets = s3.buckets.all()
@@ -86,53 +118,53 @@ if __name__ == '__main__':
         for bucket in buckets:
             location = get_location(bucket.name)
             print(SEP)
-            acl = bucket.Acl()
-            public, grants = check_acl(acl)
+            bucket_acl = bucket.Acl()
+            public, grants = check_acl(bucket_acl)
 
             if public:
                 bucket_line = termcolor.colored(
-                    bucket.name, 'blue', attrs=['bold'])
+                        bucket.name, "blue", attrs=["bold"])
                 public_ind = termcolor.colored(
-                    'PUBLIC!', 'red', attrs=['bold'])
-                termcolor.cprint('Bucket {}: {}'.format(
-                    bucket_line, public_ind))
-                print('Location: {}'.format(location))
+                        "PUBLIC!", "red", attrs=["bold"])
+                termcolor.cprint("Bucket {}: {}".format(
+                        bucket_line, public_ind))
+                print("Location: {}".format(location))
                 if grants:
                     for grant in grants:
                         permissions = grants[grant]
                         perm_to_print = [explained[perm]
                                          for perm in permissions]
-                        termcolor.cprint('Permission: {} by {}'.format(
-                            termcolor.colored(
-                                ' & '.join(perm_to_print), 'red'),
-                            termcolor.colored(groups_to_check[grant], 'red')))
+                        termcolor.cprint("Permission: {} by {}".format(
+                                termcolor.colored(
+                                        " & ".join(perm_to_print), "red"),
+                                termcolor.colored(groups_to_check[grant], "red")))
                 urls = scan_bucket_urls(bucket.name)
-                print('URLs:')
+                print("URLs:")
                 if urls:
-                    print('\n'.join(urls))
+                    print("\n".join(urls))
                 else:
-                    print('Nothing found')
+                    print("Nothing found")
             else:
                 bucket_line = termcolor.colored(
-                    bucket.name, 'blue', attrs=['bold'])
+                        bucket.name, "blue", attrs=["bold"])
                 public_ind = termcolor.colored(
-                    'Not public', 'green', attrs=['bold'])
-                termcolor.cprint('Bucket {}: {}'.format(
-                    bucket_line, public_ind))
-                print('Location: {}'.format(location))
+                        "Not public", "green", attrs=["bold"])
+                termcolor.cprint("Bucket {}: {}".format(
+                        bucket_line, public_ind))
+                print("Location: {}".format(location))
             bucketcount += 1
         if not bucketcount:
-            print('No buckets found')
-            termcolor.cprint(termcolor.colored('You are safe', 'green'))
+            print("No buckets found")
+            termcolor.cprint(termcolor.colored("You are safe", "green"))
     except botocore.exceptions.ClientError as e:
         msg = str(e)
-        if 'InvalidAccessKeyId' in msg and 'does not exist' in msg:
-            print('The Access Key ID you provided does not exist')
-            print('Please, make sure you give me the right credentials')
-        elif 'SignatureDoesNotMatch' in msg:
-            print('The Secret Access Key you provided is incorrect')
-            print('Please, make sure you give me the right credentials')
-        elif 'AccessDenied' in msg:
+        if "InvalidAccessKeyId" in msg and "does not exist" in msg:
+            print("The Access Key ID you provided does not exist")
+            print("Please, make sure you give me the right credentials")
+        elif "SignatureDoesNotMatch" in msg:
+            print("The Secret Access Key you provided is incorrect")
+            print("Please, make sure you give me the right credentials")
+        elif "AccessDenied" in msg:
             print('''Access Denied
 I need permission to access S3
 Check if the IAM user at least has AmazonS3ReadOnlyAccess policy attached
